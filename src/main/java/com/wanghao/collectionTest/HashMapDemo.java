57f6fa64a2bda49623388113fcad21c3ab4fd2cd
hao.wang <hao.wang@ericsson.com>
2019-09-02 15:25:36 +0800

    update code

---
+++ src/main/java/com/wanghao/collectionTest/HashMapDemo.java
@@ -0,0 +1,20 @@
+package com.wanghao.collectionTest;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		HashMap<String,String> hashMap = new HashMap<>();
+		hashMap.put("1", "one");
+		
+		StringBuilder sb = new StringBuilder();
+		
+		String e = "";
+		
+		Boolean f = true; 
+		
+		new Object();
+	}
+
+}
